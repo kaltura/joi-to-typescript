ec4677926e08921c559755c68cd58af457de6440
Eran Sakal <eran@sakalim.com>
2021-06-16 09:50:52 +0300

    wip

--- src/__tests__/pattern.ts
+++ src/__tests__/pattern.ts
@@ -4,23 +4,15 @@ import { convertSchema } from '../index';
 
 describe('test `Joi.object().pattern()`', () => {
   test('`interface pattern()`', () => {
+    const itemSchema = Joi.object({
+      nestedRecordProperty: Joi.string()
+    }).label('Item');
+
     const schema = Joi.object({
-      nested: Joi.object({
-        nestedName: Joi.string()
-      }).pattern(Joi.string, Joi.object({
-        nestedRecordProperty: Joi.string()
-      }))
+      item: Joi.object({}).pattern(Joi.string, itemSchema)
     })
       .label('TestSchema')
       .description('a test schema definition')
-      .pattern(Joi.string, Joi.object({
-        recordProperty: Joi.string(),
-        recordOptionalProperty: Joi.string().optional(),
-      }))
-      .pattern(Joi.string, Joi.object({
-        recordProperty: Joi.string(),
-        recordOptionalProperty: Joi.string().optional(),
-      }));
 
     const result = convertSchema({ sortPropertiesByName: false }, schema);
     expect(result).not.toBeUndefined;
@@ -32,18 +24,52 @@ export interface TestSchema {
   name?: string;
   nested: {
     nestedName: string,
-    [x: string]: {
-    nestedRecordProperty: string
-    }
-  },
-
-  /**
-   * Unknown Property
-   */
-  [x: string]: {
-    recordProperty: string,
-    recordOptionalProperty?: string
+    [x: string]: Item
   }
 }`);
   });
+
+//   test('`interface pattern()`', () => {
+//     const schema = Joi.object({
+//       nested: Joi.object({
+//         nestedName: Joi.string()
+//       }).pattern(Joi.string, Joi.object({
+//         nestedRecordProperty: Joi.string()
+//       }))
+//     })
+//       .label('TestSchema')
+//       .description('a test schema definition')
+//       .pattern(Joi.string, Joi.object({
+//         recordProperty: Joi.string(),
+//         recordOptionalProperty: Joi.string().optional(),
+//       }))
+//       .pattern(Joi.string, Joi.object({
+//         recordProperty: Joi.string(),
+//         recordOptionalProperty: Joi.string().optional(),
+//       }));
+//
+//     const result = convertSchema({ sortPropertiesByName: false }, schema);
+//     expect(result).not.toBeUndefined;
+//
+//     expect(result?.content).toBe(`/**
+//  * a test schema definition
+//  */
+// export interface TestSchema {
+//   name?: string;
+//   nested: {
+//     nestedName: string,
+//     [x: string]: {
+//     nestedRecordProperty: string
+//     }
+//   },
+//
+//   /**
+//    * Unknown Property
+//    */
+//   [x: string]: {
+//     recordProperty: string,
+//     recordOptionalProperty?: string
+//   }
+// }`);
+//   });
 });
